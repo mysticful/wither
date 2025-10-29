3a5335881c97f381bd6133aef9442b1c378cb6f1
myst <lovedrivenbystars@gmail.com>
2025-10-28 22:25:55 -0400

    wait

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ if (/bot|crawler|spider|crawling/i.test(userAgent)) {
 
 // Windows Phone
 else if (/windows phone/i.test(userAgent)) {
-  // window.location.replace("/warning.html");
+  window.location.replace("/warning.html");
 }
 
 // Android
 else if (/android/i.test(userAgent)) {
-  // window.location.replace("/warning.html");
+  window.location.replace("/warning.html");
 }
 
 // iOS
 else if ((/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) || (navigator.maxTouchPoints && navigator.maxTouchPoints > 2 && /MacIntel/.test(navigator.platform))) {
-  // window.location.replace("/warning.html");
+  window.location.replace("/warning.html");
 }
